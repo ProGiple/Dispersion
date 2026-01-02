0968d0c261e99d2b3b8520ec55707c745f834a4d
ProGiple <ghosthostgtop@gmail.com>
2026-01-03 01:14:49 +0300

    1.0f

--- src/main/java/com/satellite/progiple/scanner/DiScanner.java
+++ src/main/java/com/satellite/progiple/scanner/DiScanner.java
@@ -1,19 +1,18 @@
 package com.satellite.progiple.scanner;
 
-import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 @UtilityClass
 public class DiScanner {
 
-    @SneakyThrows
-    public static Set<Class<?>> scanPackage(String packageName) {
+    public static Set<Class<?>> scanPackage(String packageName) throws IOException, ClassNotFoundException {
         Set<Class<?>> classes = new HashSet<>();
         String path = packageName.replace('.', '/');
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
@@ -21,22 +20,41 @@ public class DiScanner {
         Enumeration<URL> resources = classLoader.getResources(path);
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
-            File dir = new File(resource.toURI());
-            if (dir.exists()) {
+            String protocol = resource.getProtocol();
+
+            if ("file".equals(protocol)) {
+                File dir = new File(resource.getFile());
                 scanDirectory(packageName, dir, classes);
+            } else if ("jar".equals(protocol)) {
+                String jarPath = resource.getPath().substring(5, resource.getPath().indexOf("!"));
+                try (JarFile jar = new JarFile(jarPath)) {
+                    scanJar(packageName, jar, classes);
+                }
             }
         }
-
         return classes;
     }
 
-
-    private void scanDirectory(String packageName, File dir, Set<Class<?>> classes) throws ClassNotFoundException {
-        for (File file : dir.listFiles()) {
+    private static void scanDirectory(String packageName, File dir, Set<Class<?>> classes) throws ClassNotFoundException {
+        if (!dir.exists()) return;
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
             if (file.isDirectory()) {
                 scanDirectory(packageName + "." + file.getName(), file, classes);
             } else if (file.getName().endsWith(".class")) {
-                String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
+                String className = packageName + "." + file.getName().replace(".class", "");
+                classes.add(Class.forName(className));
+            }
+        }
+    }
+
+    private static void scanJar(String packageName, JarFile jar, Set<Class<?>> classes) throws ClassNotFoundException {
+        String path = packageName.replace('.', '/');
+        Enumeration<JarEntry> entries = jar.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            String name = entry.getName();
+            if (name.startsWith(path) && name.endsWith(".class") && !entry.isDirectory()) {
+                String className = name.replace('/', '.').replace(".class", "");
                 classes.add(Class.forName(className));
             }
         }
